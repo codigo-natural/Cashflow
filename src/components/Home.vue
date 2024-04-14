d678ee99f1effa09390aabd7a7876b2d1a1a08e2
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-04-14 15:50:40 -0500

    feat 🎉  create history component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,7 +18,9 @@
       </Resume>
     </template>
     <template #movements>
-      <Movements />
+      <Movements 
+        :movements="movements"
+      />
     </template>
   </Layout>
 </template>
@@ -27,7 +29,7 @@
   import Layout from "./Layout.vue"
   import Header from "./Header.vue"
   import Resume from "./Resume/index.vue"
-  import Movements from "./Movements.vue"
+  import Movements from "./Movements/index.vue"
 
   export default {
     components: {
@@ -40,6 +42,38 @@
       return {
         amount: null,
         label: 'Ahorro total',
+        movements: [
+        {
+          id: 1,
+          title: "Movimiento",
+          description: "Deposito de salario",
+          amount: "1000",
+        },
+        {
+          id: 2,
+          title: "Movimiento 1",
+          description: "Deposito de honorarios",
+          amount: "500",
+        },
+        {
+          id: 3,
+          title: "Movimiento 3",
+          description: "Comida",
+          amount: "-100",
+        },
+        {
+          id: 4,
+          title: "Movimiento 4",
+          description: "Colegiatura",
+          amount: "1000",
+        },
+        {
+          id: 5,
+          title: "Movimiento 5",
+          description: "Reparación equipo",
+          amount: "1000",
+        },
+      ],
       }
     }
   }
